ebdabdd8a6585826da2998d2134d3c059ebd351c
muhammad.hafizuddin <muhammad.hafizuddin-mhishamuddin@edag.com.my>
2022-01-07 17:04:25 +0800

    first file

---
+++ mastermind.h
@@ -0,0 +1,17 @@
+#ifndef mastermind_H
+#define mastermind_h
+
+
+void easy();
+void medium();
+void hard();
+void interface();
+void end_result();
+void success();
+void start();
+void kalah();
+
+
+
+
+#endif
